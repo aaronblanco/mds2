a36937c102c220c262edd167a378edd47f62e8b5
Tesi <trm187@inlumine.ual.es>
2020-04-12 20:12:10 +0200

    He cambiado un poco el diseño de Mi Perfil como te he dicho por whatsapp y he completado el codigo Java correspondiente

--- foro/src/main/java/com/mds2/foro/Lista_Amigo.java
+++ foro/src/main/java/com/mds2/foro/Lista_Amigo.java
@@ -3,7 +3,14 @@ package com.mds2.foro;
 import java.util.Vector;
 //import Package.Amigo;
 
-public class Lista_Amigo {
+public class Lista_Amigo extends Amigos_Mi_Perfil {
 	public Mi_perfil _unnamed_Mi_perfil_;
 	public Vector<Amigo> _list_Amigo = new Vector<Amigo>();
+	
+	public Lista_Amigo() {
+		
+		
+		
+	}
+	
 }
