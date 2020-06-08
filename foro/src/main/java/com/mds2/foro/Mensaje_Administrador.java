de3c3aa6021b298c49cb83616266bb5630d97a8b
aaronblanco <aaronba13@hotmail.com>
2020-06-08 11:36:39 +0200

    Cambios perfilUsuarioreg

--- foro/src/main/java/com/mds2/foro/Mensaje_Administrador.java
+++ foro/src/main/java/com/mds2/foro/Mensaje_Administrador.java
@@ -7,17 +7,18 @@ import com.vaadin.ui.Button.ClickEvent;
 //import Package.Mensaje_moderador;
 
 public class Mensaje_Administrador extends Mensaje_moderador {
-	//private Button _eliminar;
+	private Button _eliminar;
 	public Lista_Mensaje_V_Administrador _unnamed_Lista_Mensaje_V_Administrador_;
 	iAdministrador iAd = new DB_Main();
 	
 	public Mensaje_Administrador() {
 		super();
+		this._eliminar = eliminarMensaje;
 		
 		notificarAdminB.setVisible(false);
 		eliminarMensaje.setVisible(true);
 
-		eliminarMensaje.addClickListener(new Button.ClickListener() {
+		_eliminar.addClickListener(new Button.ClickListener() {
 			
 			//cancelar();
 			
--- foro/src/main/java/com/mds2/foro/PerfilUsuarioRegGenerico.java
+++ foro/src/main/java/com/mds2/foro/PerfilUsuarioRegGenerico.java
@@ -3,45 +3,80 @@ package com.mds2.foro;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
 
 //import Package.Lista_Ultimo_mensaje;
 
 public class PerfilUsuarioRegGenerico extends PerfilUsuarioRegGenerico_ventana{
-//	private TextField _nombre;
-//	private TextField _descripcion;
+	private Label _nombre;
+	private Label _descripcion;
 //	private Image _foto;
 //	private TextField _ultimosMsg;
-//	private Button _anadirAmigo;
-//	private Button _eliminarAmigo;
-//	private TextField _estado;
+	private Button _anadirAmigo;
+	private Button _eliminarAmigo;
+	private Label _estado;
+	protected Button _sancionarUsuario;
+	protected Button _quitarSancion;
+	protected Button _promocionarAModerador;
+	protected Button _degradarAUsuarioRegistrado;
 	public Lista_Ultimo_mensaje _unnamed_Lista_Ultimo_mensaje_;
+	iUsuario iUsr = new DB_Main();
 	
 	public PerfilUsuarioRegGenerico() {
+		this._nombre = userName;
+		this._descripcion = description;
+		this._anadirAmigo = addFriend;
+		this._sancionarUsuario = sancionar;
+		this._quitarSancion = eliminarSancion;
+		this._degradarAUsuarioRegistrado = degradar;
+		this._estado = visibilidad;
+		this._promocionarAModerador = ascender;
+		this._eliminarAmigo = eliminarAmigo;
 		
-		interaccionarAmigoUsuario.setVisible(true);
 		
-		interaccionarModeracionAdmin.setVisible(false);
 		
-		interaccionarSancionAdmin.setVisible(false);
+		_anadirAmigo.setVisible(true);
 		
-		interaccionarAmigoUsuario.addClickListener(new Button.ClickListener() {
+		_sancionarUsuario.setVisible(false);
+		_quitarSancion.setVisible(false);
+		_degradarAUsuarioRegistrado.setVisible(false);
+		_estado.setVisible(false);
+		
+		
+		_anadirAmigo.addClickListener(new Button.ClickListener() {
 			
 			@Override
 			public void buttonClick(ClickEvent event) {
 				// TODO Auto-generated method stub
-			//	navigator.navigateTo("crearSeccion");
-				UI.getCurrent().getNavigator().navigateTo("interaccionarAmigoUsuario");
+			
+				anadirAmigo();
 			}
 			
 		});
 		
+		
+		_eliminarAmigo.addClickListener(new Button.ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+			
+				eliminarAmigo();
+			}
+			
+		});
+		
+		
+		
+		
+		
 	}
 	
 	public void anadirAmigo() {
-		throw new UnsupportedOperationException();
+		iUsr.anadirAmigo(aIdUsuario, aIdAmigo);
 	}
 
 	public void eliminarAmigo() {
-		throw new UnsupportedOperationException();
+		iUsr.eliminarAmigo(aIdUsuario, aIdAmigo);
 	}
 }
