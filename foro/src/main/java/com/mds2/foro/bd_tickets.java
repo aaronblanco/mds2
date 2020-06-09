fe1ec5f5d4309df14f3ea50f8774aaf3cc07c4ee
Tesi <trm187@inlumine.ual.es>
2020-06-09 11:43:03 +0200

    bd_ticket

--- foro/src/main/java/com/mds2/foro/bd_tickets.java
+++ foro/src/main/java/com/mds2/foro/bd_tickets.java
@@ -1,9 +1,12 @@
 package com.mds2.foro;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Vector;
 
 import org.orm.PersistentException;
+import org.orm.PersistentTransaction;
+import org.orm.criteria.IntegerExpression;
 
 public class bd_tickets {
 	public Bd_principal _bd_principal_tickets;
@@ -11,20 +14,49 @@ public class bd_tickets {
 
 	public List cargarTicket(int aIdUsuario) throws PersistentException {
 		Usuarios u = com.mds2.foro.UsuariosDAO.getUsuariosByORMID(aIdUsuario);
+		TicketCriteria tc = new TicketCriteria();
 		
+		tc.idUsuarioPropietarioTicket.eq(aIdUsuario);
+		Ticket[] t = com.mds2.foro.TicketDAO.listTicketByCriteria(tc);
 		
+		for(Ticket a : t) {
+			_contiene_tickets.add(a);
+		}
+		return _contiene_tickets;
 		
 	}
 
-	public boolean crearTicket(String aCuerpoTicket, int aIdUsuarioPropietarioTicket) {
-		throw new UnsupportedOperationException();
+	public boolean crearTicket(String aCuerpoTicket, int aIdUsuarioPropietarioTicket) throws PersistentException {
+		PersistentTransaction t = com.mds2.foro.MDS11920PFBlancoRoblesPersistentManager.instance().getSession().beginTransaction();
+		try {
+			Ticket ti = com.mds2.foro.TicketDAO.createTicket();
+			Usuarios u = com.mds2.foro.UsuariosDAO.getUsuariosByORMID(aIdUsuarioPropietarioTicket);
+			ti.setAbierto(true);
+			ti.setCerrado(false);
+			ti.setIdUsuarioPropietarioTicket(aIdUsuarioPropietarioTicket);
+			ti.setTexto(aCuerpoTicket);
+			
+			com.mds2.foro.TicketDAO.save(ti);
+			
+			t.commit();
+			
+			return true;
+		}
+		catch(Exception e) {
+				t.rollback();	
+				return false;
+			}
 	}
 
-	public boolean cerrarTicket(int aIdTicket) {
-		throw new UnsupportedOperationException();
+	public boolean cerrarTicket(int aIdTicket) throws PersistentException {
+		Ticket t = com.mds2.foro.TicketDAO.getTicketByORMID(aIdTicket);
+		t.setAbierto(false);
+		t.setCerrado(true);
+		return t.getCerrado();
 	}
 
-	public boolean eliminarTicket(int aIdTicket) {
-		throw new UnsupportedOperationException();
+	public boolean eliminarTicket(int aIdTicket) throws PersistentException {
+		Ticket t = com.mds2.foro.TicketDAO.getTicketByORMID(aIdTicket);
+		return com.mds2.foro.TicketDAO.delete(t);
 	}
 }
