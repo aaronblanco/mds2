0ce2ef8ea5b76d04f15eb8c50c909b6704d360e1
Tesi <trm187@inlumine.ual.es>
2020-06-09 12:09:07 +0200

    bd_imagenes y bd_media

--- foro/src/main/java/com/mds2/foro/bd_anuncios.java
+++ foro/src/main/java/com/mds2/foro/bd_anuncios.java
@@ -1,29 +1,50 @@
 package com.mds2.foro;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
+
+import org.orm.PersistentException;
 
 public class bd_anuncios {
 	public Bd_principal _bd_principal_anuncios;
 	public Vector<Anuncio> _contiene_anuncios = new Vector<Anuncio>();
 
-	public List cargarAnuncioDisponible(boolean aEstado) {
-		throw new UnsupportedOperationException();
+	public List cargarAnuncioDisponible(boolean aEstado) throws PersistentException {
+		AnuncioCriteria ac = new AnuncioCriteria();
+		
+		Anuncio[] ad = com.mds2.foro.AnuncioDAO.listAnuncioByQuery("Estado = '"+aEstado+"'", null);
+		List anDi = new ArrayList();
+		for(Anuncio a : ad)
+			anDi.add(a);
+		return anDi;
 	}
 
-	public List cargarAnuncioPublicado(boolean aEstado) {
-		throw new UnsupportedOperationException();
+	public List cargarAnuncioPublicado(boolean aEstado) throws PersistentException {
+		return cargarAnuncioDisponible(aEstado);
 	}
 
-	public boolean subirAnuncio(String aAnuncioURL) {
-		throw new UnsupportedOperationException();
+	public boolean subirAnuncio(String aAnuncioURL) throws PersistentException {
+		
+		Anuncio a = com.mds2.foro.AnuncioDAO.createAnuncio();
+		a.setImagen(aAnuncioURL);
+		a.setPublicado(false);
+		com.mds2.foro.AnuncioDAO.save(a);
+		
+		return true;
+		
 	}
 
-	public boolean anadirAnuncio(int aIdAnuncio, boolean aPublicado) {
-		throw new UnsupportedOperationException();
+	public boolean anadirAnuncio(int aIdAnuncio, boolean aPublicado) throws PersistentException {
+		
+		Anuncio a = AnuncioDAO.getAnuncioByORMID(aIdAnuncio);
+		a.setPublicado(true);
+		return a.getPublicado();
 	}
 
-	public boolean eliminarAnuncio(int aIdAnuncio, boolean aPublicado) {
-		throw new UnsupportedOperationException();
+	public boolean eliminarAnuncio(int aIdAnuncio, boolean aPublicado) throws PersistentException {
+		Anuncio a = AnuncioDAO.getAnuncioByORMID(aIdAnuncio);
+		a.setPublicado(false);
+		return !a.getPublicado();
 	}
 }
--- foro/src/main/java/com/mds2/foro/bd_imagenes.java
+++ foro/src/main/java/com/mds2/foro/bd_imagenes.java
@@ -2,11 +2,17 @@ package com.mds2.foro;
 
 import java.util.Vector;
 
+import org.orm.PersistentException;
+
 public class bd_imagenes {
 	public Bd_principal _bd_principal_imagenes;
 	public Vector<imagenes> _contiene_imagenes = new Vector<imagenes>();
 
-	public boolean crearImg(String aFotoURL) {
-		throw new UnsupportedOperationException();
+	public boolean crearImg(String aFotoURL) throws PersistentException {
+		
+		imagenes i = imagenesDAO.createImagenes();
+		i.setUrl(aFotoURL);
+		return com.mds2.foro.imagenesDAO.save(i);
+		
 	}
 }
