b49a6ebcde1e5535cc9b0de3ca83e70a184c876e
Tesi <trm187@inlumine.ual.es>
2020-06-13 11:24:24 +0200

    afnaewoi

--- foro/src/main/java/com/mds2/foro/VerPerfilGenerico.java
+++ foro/src/main/java/com/mds2/foro/VerPerfilGenerico.java
@@ -1,15 +1,16 @@
 package com.mds2.foro;
 
 import com.vaadin.navigator.View;
+import com.vaadin.ui.Button;
 
 public class VerPerfilGenerico  extends VerPerfilGenerico_ventana implements View{
-	//private Button _verPerfilUsuairo;
+	private Button _verPerfilUsuairo;
 	public ME_UsuarioReg _unnamed_ME_UsuarioReg_;
 
 	
 	public VerPerfilGenerico() {
 		
-		
+		this._verPerfilUsuairo = irPerfil;
 		
 	}
 	
